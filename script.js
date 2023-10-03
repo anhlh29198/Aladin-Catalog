3ec5bcae13cab5cc88356c17a5c39ba51b7e1b04
Anhlh <anhlh@aladintech.co>
2023-10-03 18:07:52 +0700

    add a onload check for the sidebar add content to the other html files

--- script.js
+++ script.js
@@ -1,14 +1,37 @@
 const hamburgerMenu = document.querySelector(".hamburgerMenu");
-const hamburgerLine = document.querySelectorAll(".hamburgerMenu span");
-const sidebar = document.querySelector(".sidebar");
-const main = document.querySelectorAll("section, footer");
 
 hamburgerMenu.addEventListener("click", () => {
+    const hamburgerLine = document.querySelectorAll(".hamburgerMenu span");
     hamburgerLine.forEach((line) => {
         line.classList.toggle("active");
     });
+
+    const sidebar = document.querySelector(".sidebar");
     sidebar.classList.toggle("active");
+
+    const main = document.querySelectorAll("section, footer");
     main.forEach(section => {
         section.classList.toggle("active");
     });
 });
+
+const sidebarMenu = document.querySelectorAll(".sidebarMenu");
+
+function checkPage() {
+    switch (document.location.href) {
+        case "http://127.0.0.1:5500/index.html":
+            sidebarMenu[0].classList.toggle("active");
+            break;
+        case "http://127.0.0.1:5500/products.html":
+            sidebarMenu[1].classList.toggle("active");
+            break;
+        case "http://127.0.0.1:5500/organization.html":
+            sidebarMenu[2].classList.toggle("active");
+            break;
+        case "http://127.0.0.1:5500/contact.html":
+            sidebarMenu[3].classList.toggle("active");
+            break;
+    }
+}
+
+window.onload = checkPage;
