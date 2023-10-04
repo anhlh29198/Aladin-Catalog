a68ecb430a7a77cbeb1221d10563e0bad0b3c0c7
Anhlh <anhlh@aladintech.co>
2023-10-04 18:11:01 +0700

    add touch slider to technology section add more content in products

--- script.js
+++ script.js
@@ -41,5 +41,81 @@ function checkPage() {
     
 }
 window.onload = checkPage;
+//technology logo touch slider
+const sliders = document.querySelectorAll(".slider");
+sliders.forEach(slider => {
+    const slides = Array.from(slider.querySelectorAll("div"));
 
-// function nextPartner
+    console.log(slider.getBoundingClientRect().width);
+    console.log(window.innerWidth);
+
+    let isDragging = false;
+    let startPos = 0;
+    let currentTranslate = 0;
+    let prevTranslate = 0;
+    let animationID;
+    let currentIndex = 0;
+
+    slides.forEach((slide, index) => {
+        const slideImage = slide.querySelector("img");
+        // disable default image drag
+        slideImage.addEventListener("dragstart", (e) => e.preventDefault());
+        // pointer events
+        slide.addEventListener("pointerdown", pointerDown(index));
+        slide.addEventListener('pointerup', pointerUp);
+        slide.addEventListener('pointerleave', pointerUp);
+        slide.addEventListener('pointermove', pointerMove);
+    });
+    // make responsive to changes
+    slider.addEventListener("resize", setPositionByIndex);
+
+    function pointerDown(index) {
+        return function (event) {
+            currentIndex = index;
+            startPos = event.clientX;
+            isDragging = true;
+            animationID = requestAnimationFrame(animation);
+            slider.classList.add('grabbing');
+        }
+    }
+
+    function pointerMove(e) {
+        if (isDragging) {
+            const currentPos = e.clientX;
+            currentTranslate = prevTranslate + currentPos - startPos;
+        }
+    }
+
+    function pointerUp() {
+        cancelAnimationFrame(animationID);
+        isDragging = false;
+        const movedBy = currentTranslate - prevTranslate;
+        
+        // if moved enough negative then snap to next slide if there is one
+        if (movedBy < -100 && currentIndex < slides.length - 1) currentIndex++;
+    
+        // if moved enough positive then snap to previous slide if there is one
+        if (movedBy > 100 && currentIndex > 0) currentIndex--;
+        
+        setPositionByIndex();
+    
+        slider.classList.remove('grabbing');
+    } 
+
+    function animation() {
+        setSliderPosition();
+        if (isDragging) requestAnimationFrame(animation);
+    }
+    
+    function setPositionByIndex() {
+        console.log(-slider.getBoundingClientRect().width);
+        console.log(-window.innerWidth);
+        currentTranslate = currentIndex * -slider.getBoundingClientRect().width;
+        prevTranslate = currentTranslate;
+        setSliderPosition();
+    }
+    
+    function setSliderPosition() {
+        slider.style.transform = `translateX(${currentTranslate}px)`;
+    }
+});
